62feef5a2147b6191989cf54d3e0f71f3e3fab98
Robert Smith <r.reed.smith@gmail.com>
2023-10-04 19:54:48 -0600

    Got through Functions

--- src/functions.js
+++ src/functions.js
@@ -2,7 +2,9 @@
 // Ex.:
 //   divide(10, 2);
 //   => 5
-function divide(x, y) {}
+function divide(x, y) {
+  return x/y
+}
 
 // Return the average of x and y.
 // Here, the average of x and y is the *mean* of x and y. In other words, it's
@@ -10,39 +12,104 @@ function divide(x, y) {}
 // Ex.:
 //   average(10, 2);
 //   => 6
-function average(x, y) {}
+function average(x, y) {
+  return (x + y)/2
+}
 
 // Return true if x and y are mostly equivalent up to a tolerance of 0.001.
 // In other words, return true if the aboslute value of x - y is less than 0.001.
 // Ex.:
 //   approximatelyEqual(10.001, 10);
 //   => true
-function approximatelyEqual(x, y) {}
+function approximatelyEqual(x, y) {
+  if ((x-y)<.001 || (y-x)<.001) {
+
+    return true
+
+  } else {
+    return false
+  }
+
+}
 
 // Given a first name and last name, return a full name in the format "FIRST LAST"
 // Ex.:
 //   fullName('John', 'Doe');
 //   => 'John Doe'
-function fullName(firstName, lastName) {}
+function fullName(firstName, lastName) {
+  return firstName + " " + lastName
+}
 
 // Generate the sentence "PERSON was drinking BEVERAGE at LOCATION" using the
 // person, beverage and location provided.
 // Ex.:
 //   generateSentence('Kay', 'coffee', 'the local cafe');
 //   => 'Kay was drinking coffee at the local cafe.'
-function generateSentence(person, beverage, location) {}
+function generateSentence(person, beverage, location) {
+
+  return person + " was drinking " + beverage + " at " + location + '.'
+
+}
 
 // Return the given string with all vowels replced with '*' characters.
 // Ex.:
 //   censorVowels('javascript');
 //   => 'j*v*scr*pt'
-function censorVowels(string) {}
+function censorVowels(string) {
+  
+  let vowels = ["a","e","i","o","u","A","E","I","O","U"]
+  let wordArr = []
+
+  for (let i = 0; i < string.length; i++) {
+    
+    console.log(string.length)
+    
+    if (vowels.includes(string[i])) {
+      wordArr.push('*')
+    } else {
+      wordArr.push(string[i])
+    }
+
+    
+  }
+  let newWord = "";
+  for(let letter of wordArr) {
+      newWord += letter
+  }
+
+  return newWord
+}
 
 // Return the given string in sticky case.
 // Ex.:
 //   stickyCase('hello world');
 //   => 'hElLo wOrLd'
-function stickyCase(string) {}
+function stickyCase(string) {
+
+  let wordArr = []
+
+  for (let i = 0; i < string.length; i++) {
+    
+    // console.log(string.length)
+    
+    if (i % 2 === 1) {
+      let lettr = string[i]
+
+      wordArr.push(string[i].toUpperCase())
+    } else {
+      wordArr.push(string[i])
+    }
+
+    
+  }
+  let newWord = "";
+  for(let letter of wordArr) {
+      newWord += letter
+  }
+
+  return newWord
+
+}
 
 // Return the given string in leetspeak. Leetspeak is a modified version of
 // English where characters are replaced by numbers or symbols. For this
@@ -56,7 +123,44 @@ function stickyCase(string) {}
 // Ex.:
 //   leetspeak('javascript');
 //   => 'j4v45cr1p7'
-function leetspeak(string) {}
+function leetspeak(string) {
+
+  let wordArr = []
+
+  for (let i = 0; i < string.length; i++) {
+    
+    // console.log(string.length)
+    
+    if (string[i] === "a") {
+      wordArr.push("4")
+
+    } else if (string[i] === "e") {
+      wordArr.push("3")
+    } else if (string[i] === "i") {
+      wordArr.push("1")
+    } else if (string[i] === "o") {
+      wordArr.push("0")
+    } else if (string[i] === "s") {
+      wordArr.push("5")
+    } else if (string[i] === "t") {
+      wordArr.push("7")
+
+    } else {
+      wordArr.push(string[i])
+    }
+
+    
+  }
+  let newWord = "";
+  for(let letter of wordArr) {
+      newWord += letter
+  }
+
+  return newWord
+
+
+
+}
 
 export {
   approximatelyEqual,
